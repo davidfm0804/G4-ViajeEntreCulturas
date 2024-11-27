0239a0efdc327bb63526889e20f21aa29e4f377e
Merroji <156344132+merroji@users.noreply.github.com>
2024-11-27 20:55:40 +0100

    SPRINT 1 | Celia | DWEC | Pruebas Coordenadas -> Validacion From Add Culture -> In progress [v5]

--- 01ValidarForm/V2/mostrarDatos.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo 'Pais: '.$_POST['pais'].' - ';
-    echo 'Descripcion: '.$_POST['descrip'].' - ';
-    echo 'Imagen: '.$_POST['img'].' - ';
-    /*echo 'CordX: '.$_POST['cordX'].'<br>';
-    echo 'CordY: '.$_POST['cordY'].'<br>'; */
-?>
