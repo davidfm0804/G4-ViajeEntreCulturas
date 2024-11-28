2ff36024ea21f2294b200fa754ea6cf82b59e332
celia.evg <2DAW01@esvirgua.es>
2024-11-28 13:19:46 +0100

    SPRINT 1 | Celia | DWEC | Validacion Form Add Culture -> In progress [v9]

--- 01ValidarForm/V3/coordenadas.js
+++ 01ValidarForm/V3/coordenadas.js
@@ -13,13 +13,18 @@ document.getElementById('mainMapa').addEventListener('click', function(event) {
     if (chinchetaAnterior) 
         chinchetaAnterior.remove();
 
+    // Paso de px a REM
+    $medidaRem = [];
+    $medidaRem = calculoRem(x,y);
+    console.log($medidaRem);
+
     // Crear y posicionar la imagen en las coordenadas del click
     const img = document.createElement("img");
     img.id = "chincheta";
     img.src = "./img/chincheta.png";
     img.style.position = "absolute";
-    img.style.left = `${x}px`;
-    img.style.top = `${y}px`;
+    img.style.left = `${medidaRem[0]}rem`;
+    img.style.top = `${medidaRem[1]}rem`;
     img.style.transform = "translate(-17%, -83%)";
     document.body.appendChild(img);
 });
@@ -30,3 +35,11 @@ document.addEventListener('keydown', function(event) {
         window.location.href = './form.html';
     }
 });
+
+/*-- Paso de PX a REM --*/
+function calculoRem ($Xpx, $Ypx){
+    $coordRem = [];
+    $coordRem[0] = $Xpx*0.063;
+    $coordRem[1] = $Ypx*0.063;
+    return $coordRem;
+}
