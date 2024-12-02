b2e5ec5a99d82652124ff32d7faff05622e4a3c9
irene <irene@gmail.com>
2024-12-02 21:37:01 +0100

    SPRINT1 | Celia | DWEC | Merged [aux_dwec - cmoruno_sprint1]

--- 00Pruebas/01PruebaFondoBlanco/coordenadas.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('click', function(event) {
-    const x = event.clientX;
-    const y = event.clientY;
-    document.getElementById('coordenadas').textContent = `Coordenadas: (${x}, ${y})`;
-});
