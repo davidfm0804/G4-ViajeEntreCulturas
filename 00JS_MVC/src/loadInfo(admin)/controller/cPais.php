193a8ce3488d3883afaa284d05cd63397a87b6cb
Merroji <156344132+merroji@users.noreply.github.com>
2024-12-11 09:46:07 +0100

    SPRINT3 | Celia | DWES | Ini Estructura [v8]

--- 00JS_MVC/src/loadInfo(admin)/controller/cPais.php
+++ 00JS_MVC/src/loadInfo(admin)/controller/cPais.php
@@ -1,74 +1,58 @@
 <?php
 
-require_once MODELO.'mPais.php';
+require_once MODELOS.'mPais.php';
 
-class cPais {
+class Cpais {
 
     public $tituloPagina;
-    public $view;
+    public $vista;
 
     public function __construct() {
-        $this->view = 'listadoPaises'; //Hay que cambiarle el nombre. Es la vista por defecto que mostraremos de la pagina index.php
+        $this->vista = ''; //Hay que cambiarle el nombre. Es la vista por defecto que mostraremos de la pagina index.php
         $this->tituloPagina = '';
         $this->objPais = new mPais(); //objPais es el nombre del objeto instanciado de la clase modelo Pais (mPais). Creamos objeto
     }
 
-    public function listadopaises(){
-        $this->tituloPagina = 'Listado de países';
-        return $this->objPais->selectPaises();
+    public function cListadoPaises(){ //Este método devuelve el listado de países
+        $this->tituloPagina = 'Listado de países de '.$_GET['nombreCont'];
+        $this->vista = 'listadoPaises';
+        return $this->objPais->mListadoPaises();
     }
 
-    public function mapaChincheta(){
-        $this->view = 'mapaChincheta';
+    public function cMapaChincheta(){ //Este método devuelve la vista del mapa para colocar la chincheta
+        $this->vista = 'mapaChincheta'; 
     }
 
-    public function formAlta(){
-        $this->view = 'formAlta';
+    public function cFormAltaPais(){
+        $this->vista = 'formAltaPais';
+        return $this->objPais->mFormAltaPais(); //Este método devuelve la vista del formulario para dar de alta un nuevo país
     }
 
-    public function insertDatos(){
-        return $this->objPais->insertPais();
+
+    public function cAltaPais() {
+        return $this->objpais->mAltaPais();
     }
 
-    public function obtenerCoord(){
-        $coordenadasJson = $this->objPais->getCoord();
-        
-        header('Content-Type: application/json');
+    public function cEliminarPais($nombrePais) {
+        return $this->objMpais->mBorrarPais($nombrePais);
+    }
+    
 
-        echo $coordenadasJson;
-        exit; //Si no pongo exit, no se acaba la asincronia del fetch
+    public function cObtenerPaises() {
+        return $this->objMpais->mObtenerPaises();
     }
 
-    public function borrarPais(){
-        $result = $this->objPais->eliminarPais();
-        
-        if ($result) {
-           echo "Registro eliminado correctamente";
-        } else {
-            echo "Error al eliminar el registro";
+    public function cObtenerPaisPorNombre($nombrePais) {
+        return $this->objMpais->mObtenerPaisPorNombre($nombrePais);
+    }     
+    
+    public function cModificarPais($nombrePais, $nuevoNombrePais, $bandera, $coordX, $coordY) {
+        if ($this->objMpais->mModificarPais($nombrePais, $nuevoNombrePais, $bandera, $coordX, $coordY)) {
+            return true;
         }
-        exit;
+        return false;
     }
+    
 
-    public function formModPais(){
-        $this->view = 'formModPais';
-       return $this->objPais->selectModPais();   
-    }
-
-    public function cambiarChincheta(){
-        $this->view = 'cambiarChincheta';
-    }
-
-    public function modificarPais(){
-        $result = $this->objPais->updatePais();
-
-        if ($result) {
-            echo "Registro modificado correctamente";
-         } else {
-             // Si hubo un error, establecemos otro mensaje en la sesión
-             echo "Error al modificar el registro";
-         }
-         exit;
-    }
 }
 ?>
--- 00JS_MVC/src/loadInfo(admin)/model/mPais.php
+++ 00JS_MVC/src/loadInfo(admin)/model/mPais.php
@@ -1,12 +1,16 @@
 <?php
-
 class mPais {
-
-    private $tabla = 'pais'; //Nombre de la tabla donde buscamos información
+    private $tabla = 'pais';
     private $conexion;
 
     public function __construct() {
+        require_once CONFIG.'configDbLocal.php';
+        $this->conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
+        $this->conexion->set_charset("utf8");
 
+        if ($this->conexion->connect_error) {
+            die("Conexión fallida: " . $this->conexion->connect_error);
+        }
     }
 
     public function conectar(){
@@ -14,101 +18,53 @@ class mPais {
         $this->conexion = $objetoBD->conexion; //Llamamos al metodo que realiza la conexion a la BBDD
     }
 
-    public function selectPaises(){
-        $this->conectar(); //Llamo al metodo conectar de arriba
-        $sql = 'SELECT idPais, nombrePais, bandera FROM '.$this->tabla; //Escribimos la consulta
+    public function mListadoPaises(){
+        $this->conectar();
+        $idContinente = $_GET['id'];
+        $sql = 'SELECT idPais, nombrePais, bandera FROM '.$this->tabla.' WHERE idContinente = '.$idContinente;
         $resultado = $this->conexion->query($sql); //La mandamos a la BBDD y recibimos el resultado
         return $resultado->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function insertPais(){
+    public function mFormAltaPais(){
         $this->conectar();
-        $nombrePais = $_POST['pais'];
-        $coordX = $_POST['coordX'];
-        $coordY = $_POST['coordY'];
-
-        $imgBandera = $_FILES['imgBandera']['name']; //Ejemplo: india.png
-        $imgBanderaTmp = $_FILES['imgBandera']['tmp_name'];
-        $imgBanderaPath = BANDERAS.basename($imgBandera);
-
-        if (!move_uploaded_file($imgBanderaTmp, $imgBanderaPath)) {
-            echo "Error al subir la imagen.";
-        }
-
-        // Preparar
-        $stmt = $this->conexion->prepare("INSERT INTO ".$this->tabla." (nombrePais, bandera, coordX, coordY, idContinente) VALUES (?, ?, ?, ? ,1)");
-        $stmt->bind_param("ssdd", $nombrePais, $imgBandera, $coordX, $coordY);
-
-        // Ejecutar
-        $stmt->execute();
-
-        // echo "Nuevo registro creado exitosamente";
+        $idContinente = $_GET['id'];
+        $nombreCont = $_GET['nombreCont'];
+        $sql = 'SELECT * FROM categoria';
+        $resultado = $this->conexion->query($sql); //La mandamos a la BBDD y recibimos el resultado
+        return $resultado->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function updatePais(){
-        $this->conectar();
-        $pais = $_POST['pais'];
-        $coordX = $_POST['coordX'];
-        $coordY = $_POST['coordY'];
-        $idPais = $_POST['idPais'];
-
-        //Si hemos subido archivo nuevo, creo un update donde modifico el nombre del archivo en el campo bandera
-        if(isset($_FILES['imgBandera']) && is_uploaded_file($_FILES['imgBandera']['tmp_name'])){
-            $imgBandera = $_FILES['imgBandera']['name']; //Nombre del archivo
-            $imgBanderaTmp = $_FILES['imgBandera']['tmp_name'];
-            $rutaBandera = BANDERAS.basename($imgBandera);
-            // Mover IMG a Directorio
-            if (!move_uploaded_file($imgBanderaTmp, $rutaBandera)){die("Error al subir la imagen");}
-            $archivo = $imgBandera;
-        }
-
-        if(!isset($archivo))
-            $archivo = $_POST['imgBanderaActual']; // Si no hemos subido archivo, cogemos el nombre del archivo que ya teniamos
+    public function mAltaPais($nombrePais, $bandera, $coordX, $coordY) {
+        $SQL = "INSERT INTO paises (nombrePais, bandera, coordX, coordY) 
+                VALUES ('$nombrePais', '$bandera', '$coordX', '$coordY')";
         
-
-        $sql = "UPDATE ".$this->tabla." SET nombrePais = ?, coordX = ?, coordY = ?, bandera = ? WHERE codPais = ?";
-        $conxPrp = $this->conexion->prepare($sql);
-        $conxPrp->bind_param("sddss", $pais, $coordX, $coordY, $archivo, $idPais);
-        $result = $conxPrp->execute();
-        return $result;
+        return $this->conexion->query($SQL);
     }
 
-    public function getCoord(){
-        $this->conectar();
-        $sql = "SELECT coordX, coordY FROM ".$this->tabla;
-        $result = $this->conexion->query($sql);
-
-        $coordenadas = array();
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                $coordenadas[] = $row;
-            }
-        }
-
-        return json_encode($coordenadas);
+    public function mObtenerPaises() {
+        $SQL = "SELECT * FROM paises";
+        return $this->conexion->query($SQL);
     }
 
-    public function eliminarPais(){
-        $this->conectar();
-        $id = $_POST['id'];
-        $sql = "DELETE FROM ".$this->tabla." WHERE codPais = ?";
-        $stmt = $this->conexion->prepare($sql);
-        $stmt->bind_param("i", $id);
-        if ($stmt->execute()) {
-            return true;
-        } else {
-            return false;
-        }
-        $stmt->close();
+    public function mObtenerPaisPorNombre($nombrePais) {
+        $SQL = "SELECT * FROM paises WHERE nombrePais LIKE '%$nombrePais%'";
+        return $this->conexion->query($SQL);
     }
 
-    public function selectModPais(){
-        $this->conectar();
-        $id = $_GET['id'];
-        $sql = "SELECT nombrePais, bandera, coordX, coordY FROM paises WHERE codPais = ".$id;
-        $resultado = $this->conexion->query($sql); //La mandamos a la BBDD y recibimos el resultado
-        return $resultado->fetch_all(MYSQLI_ASSOC);
+    public function mModificarPais($nombrePais, $nuevoNombrePais, $bandera, $coordX, $coordY) {
+        $SQL = "UPDATE paises 
+                SET nombrePais = '$nuevoNombrePais', bandera = '$bandera', coordX = '$coordX', coordY = '$coordY' 
+                WHERE nombrePais = '$nombrePais'";
+    
+        return $this->conexion->query($SQL);
     }
     
+
+    public function mBorrarPais($nombrePais) {
+        $SQL = "DELETE FROM paises WHERE nombrePais = '$nombrePais'";
+
+        return $this->conexion->query($SQL);
+    }
 }
 ?>
