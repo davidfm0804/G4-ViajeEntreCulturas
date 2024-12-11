22fb8a84716f17b3e143e34c8a846079285fa963
dsilvaEVG <dsilvavega.guadalupe@alumnado.fundacionloyola.net>
2024-12-11 13:09:19 +0100

    11-12-2024 (6)

--- src/admin/vistas/formAltaPais.php
+++ src/admin/vistas/formAltaPais.php
@@ -20,8 +20,9 @@
                     <input type="text" name="coordenada_y" placeholder="800" disabled>
                     <br><br>
                 </div>   
+
                 <fieldset>
-                    <legend>Item:</legend>
+                    <legend>Item 1:</legend>
                     <label for="categoria">Categoría: </label>
                     <select name="categoria" id="categoria">
                         <option disabled selected></option>
@@ -47,7 +48,93 @@
                     <br/>
                     <label for="descripcion" id="descripcion<?php echo $categoria['idCategoria']; ?>">Inserte descripción: </label><br/>
                     <textarea id="descripcion1" name="descripcion"></textarea>
+                </fieldset>
 
+                <fieldset>
+                    <legend>Item 2:</legend>
+                    <label for="categoria">Categoría: </label>
+                    <select name="categoria" id="categoria">
+                        <option disabled selected></option>
+                    <?php
+                    if(count($dataToView["data"])>0){
+                    foreach($dataToView["data"] as $categoria){
+                        ?>
+                    <option value="<?php echo $categoria['idCategoria'];?>"><?php echo $categoria['nombreCat']; ?></option>
+                    <?php
+                    } 
+                } else {
+                    ?>
+                    <tr><td colspan='4'>No hay categorias disponibles</td></tr>
+                <?php
+                }
+                ?>
+                </select>
+                <br/><br/>
+                    <label for="subirfoto" id="subirBtn<?php echo $categoria['idCategoria']; ?>">
+                        Subir foto
+                        <input type="file" id="subirfoto1" name="foto1">
+                    </label> 
+                    <br/>
+                    <label for="descripcion" id="descripcion<?php echo $categoria['idCategoria']; ?>">Inserte descripción: </label><br/>
+                    <textarea id="descripcion1" name="descripcion"></textarea>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Item 3:</legend>
+                    <label for="categoria">Categoría: </label>
+                    <select name="categoria" id="categoria">
+                        <option disabled selected></option>
+                    <?php
+                    if(count($dataToView["data"])>0){
+                    foreach($dataToView["data"] as $categoria){
+                        ?>
+                    <option value="<?php echo $categoria['idCategoria'];?>"><?php echo $categoria['nombreCat']; ?></option>
+                    <?php
+                    } 
+                } else {
+                    ?>
+                    <tr><td colspan='4'>No hay categorias disponibles</td></tr>
+                <?php
+                }
+                ?>
+                </select>
+                <br/><br/>
+                    <label for="subirfoto" id="subirBtn<?php echo $categoria['idCategoria']; ?>">
+                        Subir foto
+                        <input type="file" id="subirfoto1" name="foto1">
+                    </label> 
+                    <br/>
+                    <label for="descripcion" id="descripcion<?php echo $categoria['idCategoria']; ?>">Inserte descripción: </label><br/>
+                    <textarea id="descripcion1" name="descripcion"></textarea>
+                </fieldset>
+                
+                <fieldset>
+                    <legend>Item 4:</legend>
+                    <label for="categoria">Categoría: </label>
+                    <select name="categoria" id="categoria">
+                        <option disabled selected></option>
+                    <?php
+                    if(count($dataToView["data"])>0){
+                    foreach($dataToView["data"] as $categoria){
+                        ?>
+                    <option value="<?php echo $categoria['idCategoria'];?>"><?php echo $categoria['nombreCat']; ?></option>
+                    <?php
+                    } 
+                } else {
+                    ?>
+                    <tr><td colspan='4'>No hay categorias disponibles</td></tr>
+                <?php
+                }
+                ?>
+                </select>
+                <br/><br/>
+                    <label for="subirfoto" id="subirBtn<?php echo $categoria['idCategoria']; ?>">
+                        Subir foto
+                        <input type="file" id="subirfoto1" name="foto1">
+                    </label> 
+                    <br/>
+                    <label for="descripcion" id="descripcion<?php echo $categoria['idCategoria']; ?>">Inserte descripción: </label><br/>
+                    <textarea id="descripcion1" name="descripcion"></textarea>
                 </fieldset>
                 <input type="hidden" name="idContinente" value="<?php echo $_GET['id'];?>">
                 <input type="hidden" name="nombreCont" value="<?php echo $_GET['nombreCont'];?>">
