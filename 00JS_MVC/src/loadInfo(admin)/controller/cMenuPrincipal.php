d54366a069238257858a27505abae319c37bc623
Merroji <156344132+merroji@users.noreply.github.com>
2024-12-11 09:36:24 +0100

    SPRINT3 | Celia | DWES | Ini Estructura [v6]

---
+++ 00JS_MVC/src/loadInfo(admin)/controller/cMenuPrincipal.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once MODELOS.'mMenuPrincipal.php';
+
+class cMenuPrincipal {
+    public $tituloPagina;
+    public $vista;
+
+    public function __construct() {
+
+        $this->tituloPagina = '';
+        $this->vista = '';
+        $this->objMenuPrincipal = new mMenuPrincipal(); 
+    }
+
+    public function cMenuAdmin(){
+        $this->vista = 'menuPrincipal';
+    }
+
+    public function cPaisSelecContinente(){
+        $this->vista = 'seleccioneContinente';
+        $this->tituloPagina = 'Seleccione un continente';
+        return $this->objMenuPrincipal->mPaisSelecContinente();
+    }
+
+    public function cListadoCategorias(){
+        $this->vista = 'listadoCategorias';
+        //$this->tituloPagina = 'Seleccione un continente';
+        return $this->objMenuPrincipal->mPaisSelecContinente();
+    }
+}
+?>
