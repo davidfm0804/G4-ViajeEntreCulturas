002da4464f69495508c55cbc48036fd558924be5
Merroji <156344132+merroji@users.noreply.github.com>
2024-12-12 17:34:07 +0100

    SPRINT3 | Celia | DWES | Validaciones Servidor [v9]

--- 00JS_MVC/src/admin/assets/js/controller/cCategoria.js
+++ 00JS_MVC/src/admin/assets/js/controller/cCategoria.js
@@ -1,14 +1,10 @@
-// Alta Pais | Promesa -> Fetch + FormData
-async function altaCategoria(formData) {
-
-    /*-- Declaración Variables --*/
+function validarCategoria(formData) {
     let valid = true;
-
-    /*-- Validaciones --*/
+    let error = '';
 
     // 1. Validar que el campo no esté vacío
     if (!formData.get('categoria').trim()) {
-        alert("Por favor, indique el nombre de la categoría.");
+        error = "Por favor, indique el nombre de la categoría.";
         valid = false;
     }
     
@@ -16,32 +12,42 @@ async function altaCategoria(formData) {
     else {
         const regex = /^[A-Za-záéíóúÁÉÍÓÚüÜ\s]+$/;
         if (!regex.test(formData.get('categoria').trim())) {
-            alert("El nombre de la categoría solo puede contener letras y espacios.");
+            error = "El nombre de la categoría solo puede contener letras y espacios.";
             valid = false;
         }
     }
 
     // 3. Validar la longitud mínima y máxima
     if (valid && (formData.get('categoria').trim().length < 3 || formData.get('categoria').trim().length > 50)) {
-        alert("El nombre de la categoría debe tener entre 3 y 50 caracteres.");
+        error = "El nombre de la categoría debe tener entre 3 y 50 caracteres.";
         valid = false;
     }
 
     // 4. Validar que no sea solo espacios
     if (valid && (formData.get('categoria').trim().replace(/\s/g, '').length === 0)) {
-        alert("El nombre de la categoría no puede estar compuesto solo por espacios.");
+        error = "El nombre de la categoría no puede estar compuesto solo por espacios.";
         valid = false;
     }
 
     // 5. Validar palabras prohibidas (opcional)
     const palabrasProhibidas = ["imbecil", "tonto"];
     if (valid && (palabrasProhibidas.some((palabra) => formData.get('categoria').trim().toLowerCase().includes(palabra)))) {
-        alert("El nombre contiene palabras no permitidas.");
+        error = "El nombre contiene palabras no permitidas.";
         valid = false;
     }
 
-    /*-- Confirmación del Usuario --*/ 
-    if(valid && confirm("¿Está seguro de que desea registrar esta categoría?")){
+    return { valid, error };
+}
+
+async function altaCategoria(formData) {
+    const validation = validarCategoria(formData);
+
+    if (!validation.valid) {
+        alert(validation.error);
+        return;
+    }
+
+    if (confirm("¿Está seguro de que desea registrar esta categoría?")) {
         console.log("Validaciones pasadas, verificando existencia de la categoría...");
         console.log(formData.get('categoria'));
         
@@ -50,86 +56,61 @@ async function altaCategoria(formData) {
         console.log("Resultado de la verificación:", exists);
         if (exists) {
             alert('La categoría ya existe.');
-            valid = false;
+            return;
         }
+        
+        console.log("Categoría no existe, procediendo a registrar...");
+        const response = await mAltaCategoria(formData);
 
-        /*-- Valid === TRUE | Create FormData + Add Datos + Mostrar Datos By Promesa --*/
-        if (valid) {
-            console.log("Categoría no existe, procediendo a registrar...");
-            const response = await mAltaCategoria(formData);
-
-            if (response && response.ok) {
-                console.log("Categoría registrada correctamente");
+        if (response && response.ok) {
+            const text = await response.text();
+            if (text.includes("Categoria registrada correctamente")) {
+                console.log(text);
                 return "Categoria registrada correctamente";
             } else {
-                console.log("Error al registrar la categoría");
+                alert(text);
                 return "Error al registrar la categoría";
             }
+        } else {
+            console.log("Error al registrar la categoría");
+            return "Error al registrar la categoría";
         }
     }
 }
     
 // Borrar Categoria | Promesa -> Fetch + FormData 
 async function borrarCategoria(formData) {
-    // Validación del formData [ID] || .has -> Comprueba si existe | .get -> Comprueba si tiene valor
     if (!formData.has('id') || !formData.get('id')) {
-        console.error('Error: ID de la categoría no proporcionado');
-        return false;
+        alert("Error: ID de la categoría no proporcionado");
+        return "Error al borrar la categoría";
     }
 
     const response = await mBorrarCategoria(formData);
 
     if (response && response.ok) {
-        return "Categoria borrada correctamente";
+        const text = await response.text();
+        if (text.includes("Registro eliminado correctamente")) {
+            console.log(text);
+            return "Categoria borrada correctamente";
+        } else {
+            return "Error al borrar la categoría";
+        }
     } else {
+        console.log("Error al borrar la categoría");
         return "Error al borrar la categoría";
     }
+
 }
 
-// Modicar Categoria | Promesa -> Fetch + FormData 
 async function modificarCategoria(formData) {
+    const validation = validarCategoria(formData);
 
-    /*-- Declaración Variables --*/
-    let valid = true;
-
-    /*-- Validaciones --*/
-
-     // 1. Validar que el campo no esté vacío
-    if (!formData.get('categoria').trim()) {
-        alert("Por favor, indique el nombre de la categoría.");
-        valid = false;
-    }
-    
-    // 2. Validar que solo contenga letras y espacios
-    else {
-        const regex = /^[A-Za-záéíóúÁÉÍÓÚüÜ\s]+$/;
-        if (!regex.test(formData.get('categoria').trim())) {
-            alert("El nombre de la categoría solo puede contener letras y espacios.");
-            valid = false;
-        }
+    if (!validation.valid) {
+        alert(validation.error);
+        return;
     }
 
-    // 3. Validar la longitud mínima y máxima
-    if (valid && (formData.get('categoria').trim().length < 3 || formData.get('categoria').trim().length > 50)) {
-        alert("El nombre de la categoría debe tener entre 3 y 50 caracteres.");
-        valid = false;
-    }
-
-    // 4. Validar que no sea solo espacios
-    if (valid && (formData.get('categoria').trim().replace(/\s/g, '').length === 0)) {
-        alert("El nombre de la categoría no puede estar compuesto solo por espacios.");
-        valid = false;
-    }
-
-    // 5. Validar palabras prohibidas (opcional)
-    const palabrasProhibidas = ["imbecil", "tonto"];
-    if (valid && (palabrasProhibidas.some((palabra) => formData.get('categoria').trim().toLowerCase().includes(palabra)))) {
-        alert("El nombre contiene palabras no permitidas.");
-        valid = false;
-    }
-
-    /*-- Confirmación del Usuario --*/ 
-    if(valid && confirm("¿Está seguro de que desea registrar esta categoría?")){
+    if (confirm("¿Está seguro de que desea modificar esta categoría?")) {
         console.log("Validaciones pasadas, verificando existencia de la categoría...");
         console.log(formData.get('categoria'));
         
@@ -138,21 +119,24 @@ async function modificarCategoria(formData) {
         console.log("Resultado de la verificación:", exists);
         if (exists) {
             alert('La categoría ya existe.');
-            valid = false;
+            return;
         }
 
-        /*-- Valid === TRUE | Create FormData + Add Datos + Mostrar Datos By Promesa --*/
-        if (valid) {
-            console.log("Categoría no existe, procediendo a registrar...");
-            const response = await mModificarCategoria(formData);
+        console.log("Categoría no existe, procediendo a modificar...");
+        const response = await mModificarCategoria(formData);
 
-            if (response && response.ok) {
-                console.log("Categoría registrada correctamente");
-                return true;
+        if (response && response.ok) {
+            const text = await response.text();
+            if (text.includes("Registro modificado correctamente")) {
+                console.log(text);
+                return "Categoria Modificada Correctamente";
             } else {
-                console.log("Error al registrar la categoría");
-                return false;
+                alert(text);
+                return "Error al modificar la categoría";
             }
+        } else {
+            console.log("Error al modificar la categoría");
+            return "Error al modificar la categoría";
         }
     }
 }
--- 00JS_MVC/src/admin/controller/cCategoria.php
+++ 00JS_MVC/src/admin/controller/cCategoria.php
@@ -1,78 +1,158 @@
 <?php
-require_once MODELO.'mCategoria.php';
+    require_once MODELO.'mCategoria.php';
 
-class cCategoria {
+    class cCategoria {
 
-    public $tituloPagina;
-    public $view;
+        public $tituloPagina;
+        public $view;
 
-    public function __construct() {
-        $this->view = ''; 
-        $this->tituloPagina = '';
-        $this->objCatg = new MCategoria();
-    }
-
-    public function listadoCategorias(){
-        $this->view = 'listadoCategorias';
-        $this->tituloPagina = 'Listado de Categorías';
-        return $this->objCatg->selectCategorias();
-    }
-
-    public function formAltaCatg(){
-        $this->view = 'registroCategoria';
-    }
-
-    public function insertDatos(){
-        $categoria = $_POST['categoria'];
-        validarCategoria($categoria);
-        return $this->objCatg->insertCategoria($categoria);
-    }
-
-
-    public function borrarCategoria(){
-        $idCat = $_POST['id'];
-        // VALIDACIONES
-        $result = $this->objCatg->eliminarCategoria($idCat);
-        
-        if ($result) {
-            echo "Registro eliminado correctamente";
-        } else {
-            echo "Error al eliminar el registro";
+        public function __construct() {
+            $this->view = ''; 
+            $this->tituloPagina = '';
+            $this->objCatg = new MCategoria();
         }
-        exit;
-    }
 
-    /*-- Modificar | Vista [formModCatg] + Modelo [selectModCatg]  --*/
-    public function formModCatg(){
-        $this->view = 'formModCatg';
-        return $this->objCatg->selectModCatg();   
-    }
-
-    public function modificarCategoria(){
-        $idCatg = $_POST['idCat'];
-        $nombreCat = $_POST['categoria'];
-        // VALIDACIONES
-        $result = $this->objCatg->updateCategoria($idCatg, $nombreCat);
-
-        if ($result) {
-            echo "Registro modificado correctamente";
-        } else {
-             // Si hubo un error, establecemos otro mensaje en la sesión
-            echo "Error al modificar el registro";
+        public function listadoCategorias(){
+            $this->view = 'listadoCategorias';
+            $this->tituloPagina = 'Listado de Categorías';
+            return $this->objCatg->selectCategorias();
         }
-        exit;
-    }
 
-    // Verificar si la categoría ya existe en la base de datos
-    public function csuCategoria(){
-        if (isset($_POST['categoria'])) {
+        public function formAltaCatg(){
+            $this->view = 'registroCategoria';
+        }
+
+
+        public function insertDatos(){
+            if (!isset($_POST['categoria'])) {
+                echo "Error al registrar la categoría";
+                exit;
+            }
+
+            $categoria = $_POST['categoria'];
+            $valid = $this->validarCategoria($categoria);
+
+            if ($valid['valid']) {
+                $result = $this->objCatg->insertCategoria($categoria);
+                if ($result)
+                    echo "Categoria registrada correctamente";
+                else
+                    echo "Error al registrar la categoría";
+            } else {
+                echo $valid['error'];
+            }
+
+            exit;
+        }
+
+        public function borrarCategoria(){
+            if (!isset($_POST['id']) || empty($_POST['id'])) {
+                echo "Error al eliminar el registro: ID no proporcionado.";
+                exit;
+            }
+
+            $idCat = $_POST['id'];
+            $result = $this->objCatg->eliminarCategoria($idCat);
+
+            if ($result)
+                echo "Registro eliminado correctamente";
+            else
+                echo "Error al eliminar el registro";
+            
+            exit;
+        }
+
+        /*-- Modificar | Vista [formModCatg] + Modelo [selectModCatg]  --*/
+        public function formModCatg(){
+            $this->view = 'formModCatg';
+            return $this->objCatg->selectModCatg();   
+        }
+
+        public function modificarCategoria(){
+            if (!isset($_POST['idCat']) || !isset($_POST['categoria'])) {
+                echo "Error al modificar el registro";
+                exit;
+            }
+
+            $idCatg = $_POST['idCat'];
             $nombreCat = $_POST['categoria'];
-            $result = $this->objCatg->verificarCategoria($nombreCat);
-            echo json_encode(['exists' => $result]);
-        } else {
-            echo json_encode(['exists' => false]);
+
+            $valid = $this->validarCategoria($nombreCat);
+
+            if ($valid['valid']) {
+                $result = $this->objCatg->updateCategoria($idCatg, $nombreCat);
+                if ($result)
+                    echo "Registro modificado correctamente";
+                else
+                    echo "Error al modificar el registro";
+            } else {
+                echo $valid['error'];
+            }
+
+            exit;
         }
-        exit;
+
+        // Verificar si la categoría ya existe en la base de datos
+        public function csuCategoria(){
+            if (isset($_POST['categoria'])) {
+                $nombreCat = $_POST['categoria'];
+                $result = $this->objCatg->verificarCategoria($nombreCat);
+                echo json_encode(['exists' => $result]);
+            } else {
+                echo json_encode(['exists' => false]);
+            }
+            
+            exit;
+        }
+
+        // Validar Nombre Categoría
+        private function validarCategoria($categoria) {
+            /*-- Inicializar variables --*/
+            $valid = true;
+            $error = '';
+
+            /*-- Validaciones --*/
+
+            // 1. Verificar si el campo está vacío
+            if (empty(trim($categoria))) {
+                $valid = false;
+                $error = "Por favor, indique el nombre de la categoría.";
+            }
+
+            // 2. Verificar si el campo solo contiene letras y espacios
+            if ($valid && !preg_match('/^[A-Za-záéíóúÁÉÍÓÚüÜ\s]+$/', trim($categoria))) {
+                $valid = false;
+                $error = "El nombre de la categoría solo puede contener letras y espacios.";
+            }
+
+            // 3. Verificar si el campo tiene entre 3 y 50 caracteres
+            if ($valid && (strlen(trim($categoria)) < 3 || strlen(trim($categoria)) > 50)) {
+                $valid = false;
+                $error = "El nombre de la categoría debe tener entre 3 y 50 caracteres.";
+            }
+
+            // 4. Verificar si el campo no está compuesto solo por espacios
+            if ($valid && strlen(trim($categoria)) === 0) {
+                $valid = false;
+                $error = "El nombre de la categoría no puede estar compuesto solo por espacios.";
+            }
+
+            // 5. Verificar si el campo contiene palabras no permitidas
+            $palabrasProhibidas = ["imbecil", "tonto"];
+            foreach ($palabrasProhibidas as $palabra) {
+                if ($valid && stripos(trim($categoria), $palabra) !== false) {
+                    $valid = false;
+                    $error = "El nombre contiene palabras no permitidas.";
+                }
+            }
+
+            // 6. Verificar si la categoría ya existe
+            if ($valid && $this->objCatg->verificarCategoria($categoria)) {
+                $valid = false;
+                $error = "La categoría ya existe.";
+            }
+
+            return ['valid' => $valid, 'error' => $error];
+        }
     }
-}
 ?>
